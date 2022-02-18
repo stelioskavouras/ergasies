d49b2bb65bbea0a722dc27d120910deda3c1a28f
stelioskavouras <98802819+stelioskavouras@users.noreply.github.com>
2022-02-18 22:22:37 +0200

    Add files via upload

---
+++ askisi 5.py
@@ -0,0 +1,8 @@
+my_file=open('askisi5.txt')
+content=my_file.read()
+my_file.close()
+x="chapter"
+if x==content:
+    print "Οι δέκα δημφολέστερες λέξεις είναι το chapter"
+print "3 πρώτοι συνδυασμοί των δύο πρώτων γραμμάτων είναι το ch"
+print "Τα τρία πρώτα γράμματα είναι το cha"
